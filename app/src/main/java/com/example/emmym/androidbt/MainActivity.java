65645ccb62f37ad3316188bf2052a01b9c57dd1d
jiyuhan <jiyu-han@uiowa.edu>
2018-04-27 22:52:07 -0500

    All I have been working on. It includes Firebase connection, and its notification.

--- app/src/main/java/com/example/emmym/androidbt/MainActivity.java
+++ app/src/main/java/com/example/emmym/androidbt/MainActivity.java
@@ -1,26 +1,95 @@
 package com.example.emmym.androidbt;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    public DatabaseReference eMachinesRef;
+    private static final String TAG = "MainActivity";
+    private FirebaseSnapshotObject currentSnapshot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        eMachinesRef = FirebaseDatabase.getInstance().getReference("events/");
+
+
+        /*
+        // Read from the database
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+            }
+        };
+        eMachinesRef.addChildEventListener(childEventListener);
+
+        */
+// /*
+        eMachinesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                FirebaseSnapshotObject tempSnapshot = dataSnapshot.getValue(FirebaseSnapshotObject.class);
+                if(currentSnapshot != null) {
+                    Map<String, ArrayList<String>> detectionResult = tempSnapshot.whoWasDetected(currentSnapshot);
+                    if (detectionResult != null) {
+                        Iterator it = detectionResult.entrySet().iterator();
+                        while (it.hasNext()) {
+                            Map.Entry pair = (Map.Entry)it.next();
+                            for(String name: ((ArrayList<String>)pair.getValue())) {
+                                Log.d(TAG, "Detected " + name + "'s face at " + pair.getKey() + ".");
+                            }
+                            it.remove(); // avoids a ConcurrentModificationException
+                        }
+                    }
+                }
+                currentSnapshot = tempSnapshot;
+                //TODO: if there is any changes from the previous snapshot, we need to know
+                //TODO: who was caught on camera and where the camera is located.
+                // Log.d(TAG, dataSnapshot.getValue().toString());
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, value);
+//                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
         setContentView(R.layout.activity_main);
     }
 
@@ -31,15 +100,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
         }
-        //method for bluetooth button
     }
-        public void onBTClick (View v){
-            if(v.getId() == R.id.bt_button){
-                Intent intent = new Intent(MainActivity.this, BluetoothActivity.class);
-                startActivity(intent);
-            }
+    
+    //method for bluetooth button
+    public void onBTClick (View v){
+        if(v.getId() == R.id.bt_button){
+            Intent intent = new Intent(MainActivity.this, BluetoothActivity.class);
+            startActivity(intent);
+        }
     }
-
-
 }
 
---
+++ app/src/main/java/com/example/emmym/androidbt/User.java
@@ -0,0 +1,64 @@
+package com.example.emmym.androidbt;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+
+@IgnoreExtraProperties
+public class User {
+    String name;
+    long timestamp = 0;
+
+    public User() {}
+
+    public User(String name, long timestamp) {
+        this.name = name;
+        this.timestamp = timestamp;
+    }
+
+    public String getName() {
+        if(this.name == null) return null;
+        return this.name;
+    }
+
+    public long getTimestamp() {
+        if(this.timestamp == 0) return 0;
+        return this.timestamp;
+    }
+
+    public String toString() {
+        return name + ": " + timestamp;
+    }
+
+    public boolean hasSameName(User anotherUser) {
+        if(this.name.equals(anotherUser.getName())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasSameNameAndTime(User anotherUser) {
+        if(this.name.equals(anotherUser.getName())
+                && this.timestamp == anotherUser.getTimestamp()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasSameNameAndTime(ArrayList<User> anotherUsers) {
+        for(User user: anotherUsers) {
+            if(this.hasSameNameAndTime(user)) return true;
+        }
+        return false;
+    }
+
+    public String isDetected(User oldUser) {
+        if(this.getName() == null || oldUser.getName() == null || this.getTimestamp() == 0 || oldUser.getTimestamp() == 0) return null;
+        if(this.getName().equals(oldUser.getName())
+                && this.getTimestamp() != oldUser.getTimestamp()) {
+            return this.getName();
+        }
+        return null;
+
+    }
+}
